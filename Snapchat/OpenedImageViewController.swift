22840c91068af0bc8030a13ed503c20330e3755f
Sophia Song <sophies1234@gmail.com>
2019-03-12 12:32:10 -0700

    view image

---
+++ Snapchat/OpenedImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OpenedImageViewController.swift
+//  Snapchat
+//
+//  Created by Sophia Song on 3/12/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class OpenedImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
